d09bf28434ba91a641ca64dfcac9a981a846a273
Janos Lukacs <pszichae@gmail.com>
2023-04-13 16:50:57 +0200

    responsive navbar added without functionalities

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,18 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        '@mate-academy/eslint-config-react-typescript',
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    'overrides': [
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  rules: {
+    'max-len': ['error', {
+      ignoreTemplateLiterals: true,
+      ignoreComments: true,
+    }],
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
+    }],
+    'react/jsx-filename-extension': [1, {
+      'extensions': ['.js', '.jsx']
+    }],
+    'react/jsx-filename-extension': [1, {
+      'extensions': ['.js', '.jsx', '.tsx', '.ts']
+    }],
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>test</h1>
+      <Header />
+      <h1 className="title">Welcome to Nice Gadgets store!</h1>
     </div>
   );
 };
